f0771961073c36e6923c4fdb122b37965a6df33e
DESKTOP-5DF79DM\GA3 <h1202415004@hiratagakuen.onmicrosoft.com>
2026-04-21 16:34:46 +0900

    aaa

--- ArithmeticGame/Assets/otauka/Item.cs
+++ ArithmeticGame/Assets/otauka/Item.cs
@@ -1,18 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Item : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void OnClick()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("‰Ÿ‚µ‚½");
+        UIManager.Instance.ShowPanel();
     }
 }
